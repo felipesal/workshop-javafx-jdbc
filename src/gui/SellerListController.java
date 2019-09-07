31de28b04ac82cd4c822f164d7e2c825694ea621
Felipe Salles <prof.felipe.salles@gmail.com>
2019-09-07 14:32:17 -0300

    TextField e datepicker

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -48,8 +49,8 @@ public class SellerListController implements Initializable, DataChangeListener {
 	@FXML
 	private TableColumn<Seller, String> tableColumnEmail;
 	
-	//@FXML
-	//private TableColumn<Seller, Date> tableColumnBirthDate;
+	@FXML
+	private TableColumn<Seller, Date> tableColumnBirthDate;
 
 	@FXML
 	private TableColumn<Seller, Double> tableColumnBaseSalary;
@@ -85,8 +86,8 @@ public class SellerListController implements Initializable, DataChangeListener {
 		tableColumId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumName.setCellValueFactory(new PropertyValueFactory<>("name"));
 		tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
-		//tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
-		//Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+		tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+		Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
 		tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
 		Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 
